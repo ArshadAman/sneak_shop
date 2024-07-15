f6b64e9c742c1232efd6073b822b04cdc685f8b9
Arshad Aman <arshadaman@Arshads-Mac-mini.local>
2024-07-15 14:29:25 +0530

    SneakShop

---
+++ android/app/src/main/kotlin/com/example/sneak_shop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sneak_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(children: [
+        // Logo
+
+        // Text
+        
+      ],),
+    );
+  }
+}
