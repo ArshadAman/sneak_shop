2cb27c9de86aceb1fe71381720f976c0b3c2b3dc
Arshad Aman <arshadaman@Arshads-Mac-mini.local>
2024-07-15 14:57:42 +0530

    intro page designing

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -8,13 +8,43 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: Column(children: [
-        // Logo
-
-        // Text
-        
-      ],),
-    );
+        backgroundColor: Colors.grey[300],
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Logo
+                Padding(
+                  padding: const EdgeInsets.all(35.0),
+                  child: Center(
+                    child: Image.asset(
+                      'lib/assets/images/logo.png',
+                      height: 240,
+                    ),
+                  ),
+                ),
+            
+                const SizedBox(height: 48,),
+                Text(
+                  'Just Do It',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 24,),
+                Text(
+                  'Brand new snakers for your daily needs and make your everyday special and special day memorable.',
+                  style: TextStyle(fontSize: 16, color: Colors.grey),
+                  textAlign: TextAlign.center,
+                ),
+                ElevatedButton(onPressed: (){}, child: Text("Shop Now"))
+              ],
+            ),
+          ),
+        ));
   }
 }
